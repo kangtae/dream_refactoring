9ed721ef2999656b011855ab96c5d99255101080
kangtaewan <bras1717@naver.com>
2023-03-05 13:45:30 +0900

    6-8까지 리팩토링 공부

--- ch6/6-6.js
+++ ch6/6-6.js
@@ -1,5 +1,27 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
-
-export function getDefaultOwner() {
-  return defaultOwner;
+class Person {
+	#lastName;
+	#firstName;
+	constructor(data) {
+		this.#firstName = data.lastName
+		this.#firstName = data.firstName
+	}
+	
+	get lastName() {
+		return this.#lastName;
+	}
+	
+	get firstName() {
+		return this.#firstName;
+	}
 }
+
+let defaultOwner =  new Person({ firstName: '마틴', lastName: '파울러' })
+
+function getDefaultOwner() {
+	return defaultOwner;
+}
+
+const owner = getDefaultOwner();
+owner.firstName = '강태완';
+console.log(owner.firstName);
+console.log(getDefaultOwner());
